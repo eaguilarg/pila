0afbe2c9943155de91832a57b9f239458c89be99
introduccion03 <introduccion03@lmint12.iio2.itam.mx>
2015-02-19 13:16:15 -0600

    pila

---
+++ src/Mipila/Mipila.java
@@ -0,0 +1,16 @@
+
+package Mipila;
+
+public interface Mipila <T> {
+    public T peack();
+    public void push(T elemento);
+    public T pop();
+    public boolean isEmpty();
+}
+    
+
+    
+    
+    
+    
+
---
+++ src/Mipila/Pila.java
@@ -0,0 +1,84 @@
+
+package Mipila;
+import java.util.ArrayList;
+public class Pila <T>implements Mipila <T>  {
+
+//atributos
+    ArrayList <T> a=new ArrayList <T>();
+    
+//constructor
+    public Pila(){
+        
+    }
+    
+
+ 
+ public T peack() {
+     return a.get(a.size()-1);
+ }
+ public void push(T elemento){
+     a.add(elemento);
+        
+ }
+ public T pop(){
+     return a.remove(a.size()-1);
+ }
+ public boolean isEmpty(){
+       
+     return a.isEmpty();
+ }
+ 
+ 
+ 
+ public static void main(String[] args){
+     Pila<Character> p=new Pila();
+    String elemento="((()))";
+ 
+    
+   for(int i=0;i<elemento.length();i++){
+     if(elemento.charAt(i)=='(')
+        p.push(elemento.charAt(i));
+        if(elemento.charAt(i)==')')
+            p.pop();
+    
+    }
+     if(p.isEmpty()==true)
+         System.out.println("bien");
+     else
+         System.out.println("mal");
+     
+     
+     //palindrome
+     String palindrome="anitalavalatina";
+     String n;
+     boolean resp;
+     for(int i=0;i<palindrome.length();i++)
+         p.push(palindrome.charAt(i));
+
+     StringBuilder Nuevo=new StringBuilder();
+     for(int i=0;i<palindrome.length();i++)
+        Nuevo.append(p.pop());
+     
+     System.out.println(Nuevo.toString());
+     n=Nuevo.toString();
+     
+     if(palindrome.equals(n)){
+         
+         System.out.println("Si es palindrome");
+     }
+     else{
+         
+         System.out.println("No es palindrome");
+     }
+     
+     
+ }
+
+    
+ 
+    
+    
+    
+    
+    
+}
